36684ea8d12fd29cc8bf62dafabcea6f2782f636
Sahil Birman <contact.birmansahil@gmail.com>
2023-03-08 22:41:17 -0500

    created a passenger counter

--- javascript/01_passenger_counter/script.js
+++ javascript/01_passenger_counter/script.js
@@ -1,1 +1,16 @@
-document.getElementById("count-el").innerText = 5;
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+
+let count = 0;
+
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+}
+
+function save() {
+    let entry = count + " - ";
+    saveEl.textContent += entry;
+    count = 0;
+    countEl.textContent = count;
+}
