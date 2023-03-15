c5ae63efadb0b4ec63ff445b350d7b6337a75237
Sahil Birman <contact.birmansahil@gmail.com>
2023-03-14 22:39:24 -0400

    learning decisions in js

---
+++ javascript/02_decisions/script.js
@@ -0,0 +1,19 @@
+let score = 45;
+
+if (score > 50) {
+    console.log('You passed');
+} else {
+    console.log('Try again');
+}
+
+// Logical Opertors
+
+let score1 = 55;
+let score2 = 64;
+
+if (score1 > 50 && score2 > 50) {
+    console.log('You passed');
+} else {
+    console.log('Try again');
+}
+
